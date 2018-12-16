3f54ad0c86df75471d9ec117d3ba063b8315c910
Youri Bontekoe <youribontekoe@gmail.com>
2018-12-16 16:49:52 +0100

    Heb page hidden toegevoegt
    *Je kan nu op een knop druk om de page (alleen de center sector) the verwijderen
    *Je kan de page weer terug laten komen door middel van return page te clicken

--- netflixApp/GUI/EventListerners/Eventlisteners.java
+++ netflixApp/GUI/EventListerners/Eventlisteners.java
@@ -1,9 +1,20 @@
 package netflixApp.GUI.EventListerners;
 
+import netflixApp.GUI.GUIelements.GUI;
+import netflixApp.GUI.Interface;
+
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Eventlisteners implements ActionListener {
+    private Container cont;
+
+    public Eventlisteners(Container cont) {
+        this.cont = cont;
+    }
+
+    //Overloading the constructor so it doesn't need a parameter each time
     public Eventlisteners() {
     }
 
@@ -12,16 +23,24 @@ public class Eventlisteners implements ActionListener {
 
         //Checks for button names and assigning them to an action
         switch (e.getActionCommand()){
-            case "teset but":
-                System.out.println("button 1");
+            case "reset page":
+                resetPage();
                 break;
-            case "test button":
-                System.out.println("button 2");
-                break;
-            case "testbut 2":
-                System.out.println("testbut 2");
+            case "return page":
+                returnPage();
                 break;
         }
     }
 
+    private void resetPage(){
+        Interface ui = new Interface();
+        ui.setLayoutType(2);
+        ui.createElements(cont);
+    }
+
+    private void returnPage(){
+        Interface ui = new Interface();
+        ui.setLayoutType(1);
+        ui.createElements(cont);
+    }
 }
