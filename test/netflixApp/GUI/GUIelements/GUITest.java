e1ea1940c3534a1ecf43575fee0a1c26cc29ef0f
winsonngoh <35997618+winsonngoh@users.noreply.github.com>
2019-01-17 17:38:02 +0100

    Tests
    Template unittesten

---
+++ test/netflixApp/GUI/GUIelements/GUITest.java
@@ -0,0 +1,30 @@
+package netflixApp.GUI.GUIelements;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GUITest {
+
+    @Test
+    void setPieValues() {
+        //Arange
+
+        //Act
+
+        //Assert
+
+
+    }
+
+    @Test
+    void setInfoText() {
+        //Arange
+
+        //Act
+
+        //Assert
+
+
+    }
+}
---
+++ test/netflixApp/GUI/createElementsTest.java
@@ -0,0 +1,17 @@
+package netflixApp.GUI;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class createElementsTest {
+
+    @Test
+    void createElements() {
+        //Arange
+
+        //Act
+
+        //Assert
+    }
+}
